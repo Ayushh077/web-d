2de9b082673aac5619b364ed44159c917e934820
Ayush <07ayushh@gmail.com>
2026-09-19 10:28:01 +0530

    first commit

---
+++ Ayush/src/App.jsx
@@ -0,0 +1,5 @@
+import Greet from "../../greet";
+import Button from "./Button";
+import Button from "./Button";
+import Avatar from "./avatar";
+4 
---
+++ Ayush/src/Avatar.jsx
@@ -0,0 +1,9 @@
+export default function Avatar(){
+    return ( <div>
+        <img src="https://tse4.mm.bing.net/th/id/OIP.hXWwNOQw15ZVWKlMs-xv0wHaFQ?pid=Api&P=0&h=180" alt="" />
+        <h1>Name: Ayush</h1>
+        <button>Click Me</button>
+    </div>
+    );  
+}
+
